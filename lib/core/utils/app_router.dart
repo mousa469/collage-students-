592206b0f58bdfc00da1ac96bacded2a76ff45b1
mousa469 <mohamed.mouza469@gmail.com>
2024-10-09 10:29:24 +0300

    create register view &  sign in view & app router

---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,18 @@
+import 'package:go_router/go_router.dart';
+import 'package:students_collage/features/authentication/presentation/views/sign_in_view.dart';
+
+class AppRouter {
+  static const kSignInview = '/';
+  static const kRegisterview = '/RegisterView';
+
+  static var  routes = GoRouter(routes: [
+    GoRoute(
+      path: kSignInview,
+      builder: (context, state) => const SignInView(),
+    ),
+    GoRoute(
+      path: kRegisterview,
+      builder: (context, state) => const SignInView(),
+    ),
+  ]);
+}
