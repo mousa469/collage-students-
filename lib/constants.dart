e66b9375cef057332c5eefa2f1728ce318ad53a6
mousa469 <mohamed.mouza469@gmail.com>
2024-10-09 10:09:20 +0300

    download expected packages & initialize firebase

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xff4d49e8);
+const kSecondaryColor = Color.fromARGB(255, 255, 255, 255);
+const kScaffoldColor = Color(0xffedf3ff);
