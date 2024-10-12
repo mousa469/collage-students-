040372eb98e68c34e9f89d35d2c1673ecd23064d
mousa469 <mohamed.mouza469@gmail.com>
2024-10-12 11:06:38 +0300

    auth feature ui & logic done

--- lib/features/authentication/presentation/views/widgets/sign_in_form_container.dart
+++ lib/features/authentication/presentation/views/widgets/sign_in_form_container.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:students_collage/constants.dart';
+import 'package:students_collage/core/utils/app_router.dart';
+import 'package:students_collage/core/widgets/custom_snack_bar.dart';
+import 'package:students_collage/features/authentication/presentation/manager/auth_cubit/cubit/auth_cubit.dart';
 import 'package:students_collage/features/authentication/presentation/views/widgets/sign_in_form_section.dart';
 
 class SignInFormContainer extends StatelessWidget {
@@ -7,21 +13,42 @@ class SignInFormContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      bottom: 45,
-      child: SizedBox(
-        height: 400,
-        child: AspectRatio(
-          aspectRatio: 0.77,
-          child: Container(
-            decoration: BoxDecoration(
-                color: kSecondaryColor,
-                borderRadius: BorderRadius.circular(16)),
-            child: const Padding(
-                padding: EdgeInsets.all(30.0), child: SingleChildScrollView(child: SignInFromSection())),
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is AuthSuccess) {
+          GoRouter.of(context).push(AppRouter.kHomeView, extra: state.email);
+          customSnackBar(
+              errMessage: "signed in successfully", context: context);
+        }
+        if (state is Authfailure) {
+          customSnackBar(errMessage: state.errMessage, context: context);
+        }
+      },
+      builder: (context, state) {
+        return Positioned(
+          bottom: 45,
+          child: SizedBox(
+            height: 400,
+            child: AspectRatio(
+              aspectRatio: 0.77,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: kSecondaryColor,
+                    borderRadius: BorderRadius.circular(16)),
+                child: Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: ModalProgressHUD(
+                    inAsyncCall: state is Authloading,
+                    child: const SingleChildScrollView(
+                      child: SignInFromSection(),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
